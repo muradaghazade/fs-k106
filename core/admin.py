114911e0b4adfd04d6e9bf599d9b0419a1e57519
muradaghazade <xomurad41@gmail.com>
2025-03-04 17:11:11 +0400

    m

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from core.models import *
 
-# Register your models here.
+admin.site.register([Story, Recipe, Category, Tag, Comment, Subscriber, Contact])
--- core/views.py
+++ core/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
+from core.models import *
 
 def home(request):
-    return render(request, 'index.html')
+    stories = Story.objects.order_by('-id')
+    recent_story = stories.first()
+    categories = Category.objects.all()
+    holiday_stories = Story.objects.filter(category__title='Holiday')
+    context = {
+        'stories': stories,
+        'recent_story': recent_story,
+        'categories': categories,
+        'holiday_stories': holiday_stories,
+    }
+    return render(request, 'index.html', context=context)
