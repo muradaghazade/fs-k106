610543935b75e9a1850cf11b5be0f7e0a5c8323e
muradaghazade <xomurad41@gmail.com>
2025-04-06 17:58:45 +0400

    m

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,6 @@ app_name = 'core'
 
 urlpatterns = [
     path('', home, name='home'),
+    path('story/<int:id>/', story_detail, name='story_detail'),
+    path('stories/', stories, name='stories'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from core.models import *
 
 def home(request):
-    stories = Story.objects.order_by('-id')
+    stories = Story.objects.order_by('-id')[:4]
     recent_story = stories.first()
     categories = Category.objects.all()
     holiday_stories = Story.objects.filter(category__title='Holiday')
@@ -13,3 +13,27 @@ def home(request):
         'holiday_stories': holiday_stories,
     }
     return render(request, 'index.html', context=context)
+
+
+def story_detail(request, id):
+    story = Story.objects.get(id=id)
+    categories = Category.objects.all()
+    recent_stories = Story.objects.order_by('-id')[:3]
+    tag = Tag.objects.all()
+    context = {
+        'data': story,
+        'categories': categories,
+        'recent_stories': recent_stories,
+        'tags':tag
+    }
+    return render(request, 'single.html', context=context)
+
+
+def stories(request):
+    stories = Story.objects.all()
+    categories = Category.objects.all()
+    context = {
+        'stories': stories,
+        'categories': categories,
+    }
+    return render(request, 'stories.html', context=context)
